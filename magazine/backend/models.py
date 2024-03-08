6bbbb98d7a2fec774c661961692fc5ed34285885
Neto proj <lit25111987@gmail.com>
2024-03-08 11:01:09 +0300

    correct_scheme

--- magazine/backend/models.py
+++ magazine/backend/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
 TYPE_OF_USER = (
     ('1' , 'Обычный'),
@@ -19,7 +20,7 @@ class User(AbstractUser):
 class Category(models.Model):
     id = models.AutoField(primary_key= True)
     name = models.CharField(max_length = 40, unique = True)
-    short_name = models.CharField(max_length = 15)
+    slug_name = models.SlugField(max_length = 20)
 
     class Meta:
         verbose_name = "Категория"
@@ -33,6 +34,7 @@ class Shop(models.Model):
     address = models.TextField(verbose_name = "Адрес")
     URL = models.URLField(max_length = 256)
     file_path = models.CharField(max_length = 100)
+    user = models.ForeignKey(User, related_name = "shops", on_delete = models.CASCADE)
     categories = models.ManyToManyField(Category, related_name = "shops")
 
     class Meta:
@@ -61,9 +63,9 @@ class Product_positions(models.Model):
     id = models.AutoField(primary_key= True)
     product = models.ForeignKey(Product, related_name = "prod_param",on_delete = models.CASCADE)
     shop = models.ForeignKey(Shop, related_name = "prod_pos_shop",on_delete = models.CASCADE)
-    quantity = models.IntegerField()
-    quantity_reserve = models.IntegerField()
-    price = models.FloatField()
+    quantity = models.PositiveIntegerField()
+    quantity_reserve = models.PositiveIntegerField()
+    price = models.FloatField(validators = [MinValueValidator[0.0]])
     
     class Meta:
         verbose_name = "Позиция продукта"
@@ -82,26 +84,29 @@ class Parameter(models.Model):
 
     def __str__(self):
         return self.name
-    
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "order")
-    status = models.CharField(max_length = 20)
-    prod_position = models.ManyToManyField("Product_positions", through= "Order_rec")
-
-    class Meta:
-        verbose_name = "Корзина заказа"
-
 
 STAT_OF_ORDER = (
     ('1' , 'Предзаказ'),
     ('2' , 'Оформлено'),
     ('3' , 'Обработано'),
-)
+)  
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "order")
+    prod_position = models.ManyToManyField("Product_positions", through= "Order_rec")
+    time_order = models.DateTimeField(auto_now_add = True)
+    status = models.CharField(max_length=1, choices=STAT_OF_ORDER, verbose_name = 'Статус заказа',default = '1')
+
+    class Meta:
+        verbose_name = "Корзина заказа"
+
+
+
 
 class Order_rec(models.Model):
     product_position = models.ForeignKey(Product_positions, related_name = "product_record",on_delete = models.CASCADE)
     order = models.ForeignKey(Order, related_name = "order_recs", on_delete = models.CASCADE)
-    status = models.CharField(max_length=1, choices=STAT_OF_ORDER, verbose_name = 'Статус заказа',default = '1')
+    quantity = models.PositiveIntegerField()
 
     class Meta:
         verbose_name = "Позиция заказа"
