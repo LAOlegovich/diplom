50997930e7f5e5ecb63b5377126da999f7d0f41a
Neto proj <lit25111987@gmail.com>
2024-03-13 21:41:30 +0300

    add UserManager

--- magazine/backend/models.py
+++ magazine/backend/models.py
@@ -6,6 +6,43 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 
 from django_rest_passwordreset.tokens import get_token_generator
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """
+    Миксин для управления пользователями
+    """
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 TYPE_OF_USER = (
     ('1' , 'Покупатель'),
@@ -13,6 +50,10 @@ TYPE_OF_USER = (
     ('3' ,'Администратор_площадки'),)
 
 class User(AbstractUser):
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(('email address'), unique=True)
     type = models.CharField(max_length=1, choices= TYPE_OF_USER, verbose_name = 'Тип пользователя', default = '1')
     telephone = models.CharField(max_length=20)
     city = models.CharField(max_length=40, null = True)
